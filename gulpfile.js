476843058c90e61ccffa7e4004507382a4fc3d8f
mrjleo <mrjleo@users.noreply.github.com>
2024-07-11 13:57:13 +0200

    format gulpfile with Prettier

--- gulpfile.js
+++ gulpfile.js
@@ -18,29 +18,46 @@ function _compile_latex(glob) {
     stderr: true,
     stdout: false,
   };
-  return gulp.src(glob)
-    .pipe(exec(file => `latexmk -xelatex -interaction=nonstopmode -cd -bibtex- -pdf ${file.path}`, options))
-    .pipe(exec(file => `pdf2svg ${file.path.slice(0, -4)}.pdf ${file.path.slice(0, -4)}_out.svg`, options))
-    .pipe(exec(file => `latexmk -cd -C ${file.path}`, options))
-    .pipe(exec.reporter(reportOptions))
+  return gulp
+    .src(glob)
+    .pipe(
+      exec(
+        (file) =>
+          `latexmk -xelatex -interaction=nonstopmode -cd -bibtex- -pdf ${file.path}`,
+        options
+      )
+    )
+    .pipe(
+      exec(
+        (file) =>
+          `pdf2svg ${file.path.slice(0, -4)}.pdf ${file.path.slice(
+            0,
+            -4
+          )}_out.svg`,
+        options
+      )
+    )
+    .pipe(exec((file) => `latexmk -cd -C ${file.path}`, options))
+    .pipe(exec.reporter(reportOptions));
 }
 
 gulp.task("compile_latex", () => {
   return _compile_latex(["*/img/**/*.tex", "!*/img/**/_*.tex"]);
 });
 
-gulp.task(
-  "serve",
-  () => {
-    connect.server({
-      root: root,
-      port: port,
-      host: host,
-      livereload: { port: lrport },
-    });
+gulp.task("serve", () => {
+  connect.server({
+    root: root,
+    port: port,
+    host: host,
+    livereload: { port: lrport },
+  });
 
-    // we need to use polling when running inside docker
-    gulp.watch(["*/img/**/*.tex", "!*/img/**/_*.tex"], { usePolling: true }).on("change", path => _compile_latex(path).pipe(connect.reload()));
-    gulp.watch(["*/*.html", "*/*.md", "*/*.css"], { usePolling: true }).on("change", path => gulp.src(path).pipe(connect.reload()));
-  }
-);
+  // we need to use polling when running inside docker
+  gulp
+    .watch(["*/img/**/*.tex", "!*/img/**/_*.tex"], { usePolling: true })
+    .on("change", (path) => _compile_latex(path).pipe(connect.reload()));
+  gulp
+    .watch(["*/*.html", "*/*.md", "*/*.css"], { usePolling: true })
+    .on("change", (path) => gulp.src(path).pipe(connect.reload()));
+});
